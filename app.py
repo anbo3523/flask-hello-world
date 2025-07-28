eaee7f870c45e8811e8c3290acca60ace24aa40c
anbo3523 <anbo3523@colorado.edu>
2025-07-28 18:18:55 +0000

    Add database route

--- app.py
+++ app.py
@@ -5,3 +5,9 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello, World from Harumi Booth in 3308'
+
+@app.route('/db_test')
+def testing():
+    conn = psycopg2.connect("postgresql://hello_word_database_user:9JMau27WnwffUfKOeKY3K2jjBvSbbAPL@dpg-d23rne6mcj7s739mn3sg-a/hello_word_database")
+    conn.close()
+    return "Database Connection Successful"
